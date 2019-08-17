c091f720f85b23fa0c67e915461ac40e9dc02c06
Vincent Teas <vincentteas@gmail.com>
2019-08-16 21:03:39 -0700

    added session storage

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,72 @@
-const handleSignin = (postgres, bcrypt) => (req, res) => {
+const jwt = require("jsonwebtoken");
+const redis = require("redis");
+
+const redisClient = redis.createClient(process.env.REDIS_URI);
+const checkUsernamePassword = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('Incorrect form submission');
+    return Promise.reject("Incorrect form submission");
   }
-  postgres.select('email', 'hash').from('login')
-    .where('email', '=', email)
+  return db
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return postgres.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('Unable to get user'))
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then(user => user[0])
+          .catch(err => Promise.reject("Unable to get user"));
       } else {
-        res.status(400).json('Wrong credentials')
+        Promise.reject("Wrong credentials");
       }
     })
-    .catch(err => res.status(400).json('Wrong credentials'))
-}
+    .catch(err => res.status(400).json("Wrong credentials"));
+};
+const getAuthTokenId = (res, req) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply)=> {
+    if (err || !reply) {
+      return res.status(400).json('Unauthorized')
+    }
+    return res.json({id: reply})
+  });
+};
 
+const signToken = email => {
+  const jwtPayload = { email };
+  /* return jwt.sign(jwtPayload, process.env.JWTSECRET); */
+  return jwt.sign(jwtPayload, "JTWSECRET", { expiresIn: "7 days" });
+};
+const setToken = (token, id) => {
+  return Promise.resolve(redisClient.set(token, id));
+};
+const createSessions = user => {
+  const { id, email } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: "true", userId: id, token };
+    })
+    .catch(console.log);
+};
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId(res, req)
+    : checkUsernamePassword(db, bcrypt, req, res)
+        .then(data => {
+          return data.id && data.email
+            ? createSessions(data)
+            : Promise.reject("Wrong Credentials");
+        })
+        .then(session => res.json(session))
+        .catch(err => res.status(400).json(err));
+};
 module.exports = {
-  handleSignin: handleSignin
-}
+  signinAuthentication
+};
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Heroku deployment");
 });
-app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
